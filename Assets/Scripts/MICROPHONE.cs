67f020ef8762194a9298634f3374eecd83ae1f40
AbelEnklaar <39468138+AbelEnklaar@users.noreply.github.com>
2020-02-09 18:49:10 +0000

    Mic input and averaged volume display
    Microphone script that has a demo mode with audio sources.
    
    visualiser script with public target, sample amount & normalizer.
    
    Not yet tested with microphone

---
+++ Assets/Scripts/MICROPHONE.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MICROPHONE : MonoBehaviour
+{
+    private AudioSource micTake;
+    private string ActiveMic;
+
+    public float[] samples = new float [512]; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       micTake = GetComponent<AudioSource>();
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+
+        // Start recording with built-in Microphone and play the recorded audio right away
+
+        
+        if (ActiveMic == null)
+        {
+            ActiveMic = Microphone.devices[0];
+            micTake.clip = Microphone.Start(ActiveMic, true, 5, 44100);
+            while (!(Microphone.GetPosition(ActiveMic) > 0))
+            {
+            }
+
+            micTake.Play();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        
+    }
+}
---
+++ Assets/Scripts/Visualiser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Visualiser : MonoBehaviour
+{
+    public float loudness = 0;
+    public int samples = 512;
+    public AudioSource audio;
+    public Slider slider;
+    [Range(1, 100)] public float normalizer = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        loudness = GetAverageVolume() * normalizer;
+        slider.value = loudness; 
+    }
+
+    float GetAverageVolume() 
+        {
+            float[] data = new float[samples];
+            float a = 0f;
+            audio.GetOutputData(data, 0);
+            foreach (var s in data)
+            {
+                a += Mathf.Abs(s);
+            }
+
+            return a / samples;
+            
+        }
+        
+    }
+
