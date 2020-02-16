d3792c3c1ae47f2791e48f5715690cee14cf7ea2
AbelEnklaar <39468138+AbelEnklaar@users.noreply.github.com>
2020-02-16 17:40:38 +0000

    clean up code and comments

--- Mic Check/Assets/Scripts/MICROPHONE.cs
+++ Mic Check/Assets/Scripts/MICROPHONE.cs
@@ -7,11 +7,10 @@ public class MICROPHONE : MonoBehaviour
     private AudioSource micTake;
     private string ActiveMic;
 
-    public float[] samples = new float [512]; 
-    
-    // Start is called before the first frame update
+    public float[] samples = new float [512];
     void Start()
     {
+        //look for connected microphone and print their name in the console
        micTake = GetComponent<AudioSource>();
         foreach (var device in Microphone.devices)
         {
@@ -20,24 +19,20 @@ public class MICROPHONE : MonoBehaviour
 
         // Start recording with built-in Microphone and play the recorded audio right away
 
-        
+        //check if there isn't a microphone connected, and take the first microphone and start it as audiosource micTake 
         if (ActiveMic == null)
         {
             ActiveMic = Microphone.devices[0];
+            Debug.Log(ActiveMic);
             micTake.clip = Microphone.Start(ActiveMic, true, 100, 44100);
             while (!(Microphone.GetPosition(ActiveMic) > 0))
             {
             }
-
+            
             micTake.Play();
 
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-         
-        
-    }
+   
 }
